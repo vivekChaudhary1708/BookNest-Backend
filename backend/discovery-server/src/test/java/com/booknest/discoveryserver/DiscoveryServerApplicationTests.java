77143a4c93fa3f0f555005d04c58c227c242a4b3
Vivek Raj <vivekchaudhary1708gmail.com>
2026-05-18 10:20:57 +0530

    [vivek] discovery-server code

---
+++ backend/discovery-server/src/test/java/com/booknest/discoveryserver/DiscoveryServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.booknest.discoveryserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiscoveryServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
